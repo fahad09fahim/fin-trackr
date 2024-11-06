47a015c99b1306b112cd50021076f8a0a31dd044
fahad09fahim <anik.fahim07@gmail.com>
2024-11-07 00:22:26 +0600

    connect to github

---
+++ Client/fintrackr-client/src/main.jsx
@@ -0,0 +1,27 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./components/root/Root";
+import Home from "./components/Home/Home";
+import ErrorPage from "./components/Shared/ErrorPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
