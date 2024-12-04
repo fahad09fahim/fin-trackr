56fbc1b937855d04a209b08c9c45ab17512bead3
fahad09fahim <anik.fahim07@gmail.com>
2024-12-05 02:18:59 +0600

    changes some size in navbar

--- Client/fintrackr/src/components/Shared/Header.jsx
+++ Client/fintrackr/src/components/Shared/Header.jsx
@@ -3,15 +3,17 @@ import Button from "./Button";
 
 const Header = () => {
   return (
-    <div className="px-3   bg-customBg">
-      <nav className="flex justify-between">
+    <div className=" px-6  bg-customBg">
+      <nav className="flex justify-between mx-auto ">
         {/* logo */}
-        <img src={logo} className="h-[70px]" alt="logo" />
+        <img src={logo} className="h-14" alt="logo" />
         {/* navigation items */}
         <div className="flex justify-between items-center gap-4">
           {/* <button>Login</button>
           <button>Get Start</button> */}
-          <Button variant="primary">Log in</Button>
+          <Button className="px-4 py-1" variant="primary">
+            Log in
+          </Button>
         </div>
       </nav>
     </div>
--- Client/fintrackr/src/components/Utilities/Login.jsx
+++ Client/fintrackr/src/components/Utilities/Login.jsx
@@ -3,14 +3,28 @@ import { Input } from "@/components/ui/input";
 
 const Login = () => {
   return (
-    <div className="bg-[url('https://i.ibb.co.com/h1hxcRR/bg.jpg')] bg-cover bg-center bg-no-repeat h-svh w-screen">
-      <div className="flex justify-center items-cente ">
-        <div className="grid w-full max-w-sm items-center gap-1.5">
-          <Label htmlFor="email">Email</Label>
+    // <div className="bg-[url('https://i.ibb.co.com/h1hxcRR/bg.jpg')] bg-cover bg-center bg-no-repeat h-svh w-auto">
+    <div className="flex justify-center items-center h-screen ">
+      <div className="grid w-full max-w-sm items-center gap- bg-transparent shadow-white">
+        <h1 className="text-center text-2xl">Login </h1>
+        <form action="">
+          <Label htmlFor="email">Your Email</Label>
           <Input type="email" id="email" placeholder="Email" />
+          <Label htmlFor="email">Password</Label>
+          <Input type="password" id="password" placeholder="password" />
+        </form>
+        <div className="flex justify-between mt-2">
+          <div className="flex justify-center items-center gap-1">
+            <input type="checkbox" />
+            <p>Remember Me</p>
+          </div>
+          <a href="#" className="text-sm text-primary">
+            Forgot Password?
+          </a>
         </div>
       </div>
     </div>
+    // </div>
   );
 };
 
